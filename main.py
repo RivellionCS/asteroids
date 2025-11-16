3f647493b4ab0ec215f3270129f73ec6df0bf601
RianAhmedWork <146366018+RianAhmedWork@users.noreply.github.com>
2025-11-16 00:00:09 -0400

    check player and asteroid collision

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
+import sys
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
-from logger import log_state
+from logger import log_state, log_event
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
@@ -26,6 +27,11 @@ def main():
         dt = game_clock.tick(60) / 1000
         screen.fill("black")
         updatable.update(dt)
+        for asteroid in asteroids:
+            if asteroid.collides_with(player_character):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
         for sprite in drawable:
             sprite.draw(screen)
         pygame.display.flip()
